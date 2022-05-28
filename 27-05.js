890d16eff824a864982b5ea7e214bf63b0e400ee
Denise Silva <denisekellypereira@hotmail.com>
2022-05-27 22:04:03 -0300

    Exercicio da Lis

---
+++ 27-05.js
@@ -0,0 +1,8 @@
+
+const frase1 = "Eu sou dev";
+const idade = 22;
+const formacao = 2022;
+const conta = 29.02;
+const boolean = true;
+
+console.log(`Minha formação: ${frase1}, minha idade é ${idade}, ano que me formei ${formacao}, o ano e mês do meu nascimento em decimal é ${conta}` == true);
